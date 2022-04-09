df3987400d213a512e45c2e314eb2ad903cb1470
Rigoberto Quezada <isvaku@hotmail.com>
2022-04-09 10:10:54 -0500

    Add resetPasswordRequest and resetPassword actions

--- app_node/src/Routes/User/user.ts
+++ app_node/src/Routes/User/user.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import extractJWT from "../../Middlewares/extractJWT";
 import controller from "../../Controllers/User/user";
+import expressAsyncHandler from "express-async-handler";
 
 const router = express.Router();
 
 router.get("/validate", extractJWT, controller.validateToken);
 router.post("/register", controller.register);
 router.post("/login", controller.login);
+router.post("/resetPasswordRequest", expressAsyncHandler(controller.resetPasswordRequest));
+router.post("/resetPassword", expressAsyncHandler(controller.resetPassword));
 
 export default router;
